7d8cf71ec6dc716171c6950425dad4701f03833e
Darllyson Santos <darllyson.santos11@gmail.com>
2025-02-15 18:45:38 -0300

    Criado a ação de remover o func. 'João' da Lista

--- src/Principal.java
+++ src/Principal.java
@@ -16,5 +16,10 @@ public class Principal {
         funcionarios.add(new Funcionario("Laura", LocalDate.of(1994,7,8), new BigDecimal("3017.45"), "Gerente"));
         funcionarios.add(new Funcionario("Heloísa", LocalDate.of(2003,5,24), new BigDecimal("1606.85"), "Eletricista"));
         funcionarios.add(new Funcionario("Helena", LocalDate.of(1996,9,2), new BigDecimal("2799.93"), "Gerente"));
+
+        //Removendo o funcionário João.
+        funcionarios.removeIf((funcionario) -> funcionario.getNome().equals("João"));
     }
+
+
 }
